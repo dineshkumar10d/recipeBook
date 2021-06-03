52b1b84cd59c89bdb4b3469e6a5d1522f85aaf95
DINESHKUMAR <dk1981999@gmail.com>
2021-06-03 18:18:07 +0530

    first step of the app

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes:Recipe[]=[
+    new Recipe('Test-Recipe','this is simply test','https://cdn2.vectorstock.com/i/1000x1000/77/66/recipe-word-text-typography-design-logo-icon-vector-21127766.jpg'),
+    new Recipe('Test-Recipe','this is simply test','https://cdn2.vectorstock.com/i/1000x1000/77/66/recipe-word-text-typography-design-logo-icon-vector-21127766.jpg')
 
+  ];
   constructor() { }
 
   ngOnInit(): void {
