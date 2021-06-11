cab7e4fb615b9c27ffeef3571cb18bae459c1e4a
DINESHKUMAR <dk1981999@gmail.com>
2021-06-11 19:07:52 +0530

    http methods over

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { DataStorageService } from 'src/app/shared/data-storage.service';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -12,7 +13,7 @@ import { RecipeService } from '../recipe.service';
 export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
   subcription: Subscription;
-  constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) { }
+  constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute, private dataStorageService: DataStorageService) { }
 
   ngOnInit(): void {
     this.subcription = this.recipeService.recipeChanged
@@ -20,6 +21,8 @@ export class RecipeListComponent implements OnInit, OnDestroy {
         this.recipes = recipes;
       })
     this.recipes = this.recipeService.getRecipes();
+
+    this.dataStorageService.fetchRecipes();
   }
 
   onNewRecipe() {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
@@ -6,29 +6,34 @@ import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService{
-    recipeSelected = new EventEmitter<Recipe>();
+    recipeSelected = new Subject<Recipe>();
     recipeChanged = new Subject<Recipe[]>();
-
-    private recipes:Recipe[]=[
-        new Recipe('Wheat-Halwa',
-        'Thirunelveli Halwa - tasty',
-        'https://www.jeyashriskitchen.com/wp-content/uploads/2017/10/wheat-halwa.jpg',
-        [new Ingredient('Wheat',200),
-         new Ingredient('Sugar',200),
-         new Ingredient('Cashew-nuts',100),
-         new Ingredient('ghee',100)
-        ]),
-        new Recipe('Coconut-Burfi',
-        'Coconut Parpi is simply taste',
-        'https://www.subbuskitchen.com/wp-content/uploads/2017/10/Coconut-Burfi_finalforweb2-1300x730.jpg',
-        [new Ingredient('Cocunut',1),
-         new Ingredient('sugar',200)])
-      ];
+    // private recipes:Recipe[]=[
+    //   new Recipe('Wheat-Halwa',
+    //   'Thirunelveli Halwa - tasty',
+    //   'https://www.jeyashriskitchen.com/wp-content/uploads/2017/10/wheat-halwa.jpg',
+    //   [new Ingredient('Wheat',200),
+    //    new Ingredient('Sugar',200),
+    //    new Ingredient('Cashew-nuts',100),
+    //    new Ingredient('ghee',100)
+    //   ]),
+    //   new Recipe('Coconut-Burfi',
+    //   'Coconut Parpi is simply taste',
+    //   'https://www.subbuskitchen.com/wp-content/uploads/2017/10/Coconut-Burfi_finalforweb2-1300x730.jpg',
+    //   [new Ingredient('Cocunut',1),
+    //    new Ingredient('sugar',200)])
+    // ];
+    private recipes: Recipe[]=[];
 
       constructor(private slService: ShoppingListService){}
 
+      setRecipe(recipes: Recipe[]){
+        this.recipes = recipes;
+        this.recipeChanged.next(this.recipes.slice());
+      }
       getRecipes(){
-        return this.recipes.slice();
+        console.log("Get Recipe",this.recipes.slice());
+        return this.recipes;
       }
 
       getRecipe(index: number){
@@ -42,6 +47,7 @@ export class RecipeService{
       addRecipe(recipe: Recipe){
         this.recipes.push(recipe);
         this.recipeChanged.next(this.recipes.slice());
+        console.log("addRecipe",this.recipes);
       }
 
       updateRecipe(index: number, newRecipe: Recipe){
